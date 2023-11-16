0178b4354c329f5a8f5b7f26fec6b79bda8f4680
yeowon2 <127908501+yeowon2@users.noreply.github.com>
2023-11-16 23:23:37 +0900

    [Bronze V] Title: 꼬마 정민, Time: 228 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		long x = scanner.nextLong();
+		long y = scanner.nextLong();
+		long z = scanner.nextLong();
+		
+		System.out.println(x+y+z);
+	}
+}
