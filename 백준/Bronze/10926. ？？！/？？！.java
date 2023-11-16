f21a08832c527adc533d043bc08a9006c56d8cf5
yeowon2 <127908501+yeowon2@users.noreply.github.com>
2023-11-16 14:03:40 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String x = scanner.next();
+		
+		System.out.println(x + "??!");
+	}
+}
