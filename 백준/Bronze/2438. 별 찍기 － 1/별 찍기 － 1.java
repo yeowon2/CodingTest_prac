59bbe8e37244f82a9ad7267af46ec98b101c447d
yeowon2 <127908501+yeowon2@users.noreply.github.com>
2023-11-19 22:56:00 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 228 ms, Memory: 14992 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		br.close();
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
