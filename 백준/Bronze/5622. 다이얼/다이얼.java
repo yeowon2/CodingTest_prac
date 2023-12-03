a9224ec88abc81ec58638b64f9f11429eafa35b8
yeowon2 <127908501+yeowon2@users.noreply.github.com>
2023-12-04 07:33:23 +0900

    [Bronze II] Title: 다이얼, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		int count = 0;
+		int value;
+		
+		while(true) {
+			
+			value = System.in.read();
+			
+			if(value == '\n') {
+				break;
+			}
+			
+			if(value < 68) count += 3;
+			else if(value < 71) count += 4;
+			else if(value < 74) count += 5;
+			else if(value < 77) count += 6;
+			else if(value < 80) count += 7;
+			else if(value < 84) count += 8;
+			else if(value < 87) count += 9;
+			else count += 10;
+			
+			
+		}
+		System.out.print(count);
+	}
+}
